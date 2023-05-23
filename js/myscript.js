e4f51631888698b5a1fbf235181538b02ff90e26
rc2pc2 <petriccarcc@gmail.com>
2023-05-23 12:28:37 +0200

    small refactoring on the script

--- js/myscript.js
+++ js/myscript.js
@@ -28,16 +28,3 @@ function createElement(tagName, className){
     cellElement.className = className;
     return cellElement;
 }
-
-
-
-/**
- *
- * Generare una lista usando una funzione:
- * nell'html ci dovrà essere esclusivamente un elemento ul parent,
- * attraverso una funzione creiamo un singolo elemento html di tipo li
- * poi ne aggiungiamo una decina all'ul, sempre via js.
- *
- * BONUS:
- * Se clicchiamo sul li il suo testo verrà sbarrato.
- */
